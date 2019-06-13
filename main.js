d1dedfb67cb5d9b16cbca64b4eef3c1ac573ca43
Dominique BELLO <dominique.bello@outlook.fr>
2019-06-13 12:27:34 +0200

    First Commit
    CodeCademy_Mondrians_painting_Practice

---
+++ main.js
@@ -0,0 +1,16 @@
+if (longCode) {
+	clickPlus();
+	click('Code or Text Snippet');
+	select('language');
+} else {
+	// Your
+	// Code
+	// Could
+	// Be
+	// Really
+	// Really
+	// Long
+	// In
+	// People's
+	// Feed
+}
